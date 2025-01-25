7ed3ea78aedacb4e4bdd14bba337b809a0450b6e
IsaamMJ <136082478+IsaamMJ@users.noreply.github.com>
2025-01-25 07:14:55 +0530

    Bug fixes for vercel deployment 04

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,17 +1,15 @@
-export default function UserProfile({ params }: { params: { id: string } }) {
+interface Params {
+    id: string;
+}
+
+export default function UserProfile({ params }: { params: Params }) {
     return (
-      <div className="flex flex-col items-center justify-center min-h-screen py-6 bg-gray-100 px-4">
-        <div className="w-full max-w-lg p-6 bg-white shadow-md rounded-lg">
-          <h1 className="text-3xl font-bold text-center text-gray-800 mb-4">User Profile</h1>
-          <hr className="mb-4" />
-          <p className="text-xl text-gray-700 text-center">Welcome to your profile page!</p>
-          <div className="mt-6 text-center">
-            <span className="inline-block px-4 py-2 text-white bg-orange-500 rounded-lg text-lg">
-              User ID: {params.id}
-            </span>
-          </div>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1>Profile</h1>
+            <hr />
+            <p className="text-4xl">Profile page 
+            <span className=" p-2 ml-2 rounded bg-orange-500 text-black">{params.id}</span>
+            </p>
         </div>
-      </div>
     );
-  }
-  
+}
